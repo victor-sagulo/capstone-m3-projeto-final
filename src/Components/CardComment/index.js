d819f38550734fe116507de4730597edecd2dc98
Juan Rego da Silva Rocha <juanrego.s@outlook.com>
2022-03-16 15:07:54 -0300

    feat: style mobile and desktop

--- src/Components/CardComment/index.js
+++ src/Components/CardComment/index.js
@@ -1,20 +1,68 @@
+import { useState } from "react";
 import { FaThumbsUp } from "react-icons/fa";
+import { Desktop, Mobile } from "./style";
 
 const CardComment = ({ img, name, comment, likes }) => {
+  const [width, setWidth] = useState("");
+
+  window.onresize = window.onload = () => {
+    setWidth(window.innerWidth);
+  };
+
   return (
-    <div className="card-comment">
-      <div className="img-holder">
-        <img src={img}></img>
-      </div>
-      <div className="content-holder">
-        <span className="name">{name}</span>
-        <p className="comment">{comment}</p>
-        <span className="like-holder">
-          {likes}
-          <FaThumbsUp />
-        </span>
-      </div>
-    </div>
+    <>
+      {width < 768 ? (
+        <Mobile className="card-comment">
+          <div className="img-holder">
+            <img src={img}></img>
+          </div>
+          <div className="content-holder">
+            <span className="name">{name}name</span>
+            <p className="comment">
+              {comment}Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry. Lorem Ipsum has been the industry's standard
+              dummy text ever since the 1500s, when an unknown printer took a
+              galley of type and scrambled it to make a type specimen book. It
+              has survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+            <span className="like-holder">
+              {likes}5
+              <FaThumbsUp className="like-icon" />
+            </span>
+          </div>
+        </Mobile>
+      ) : (
+        <Desktop>
+          <div className="img-holder">
+            <img src={img}></img>
+          </div>
+          <div className="content-holder">
+            <span className="name">{name}name</span>
+            <p className="comment">
+              {comment}Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry. Lorem Ipsum has been the industry's standard
+              dummy text ever since the 1500s, when an unknown printer took a
+              galley of type and scrambled it to make a type specimen book. It
+              has survived not only five centuries, but also the leap into
+              electronic typesetting, remaining essentially unchanged. It was
+              popularised in the 1960s with the release of Letraset sheets
+              containing Lorem Ipsum passages, and more recently with desktop
+              publishing software like Aldus PageMaker including versions of
+              Lorem Ipsum.
+            </p>
+            <span className="like-holder">
+              {likes}5
+              <FaThumbsUp className="like-icon" />
+            </span>
+          </div>
+        </Desktop>
+      )}
+    </>
   );
 };
 
