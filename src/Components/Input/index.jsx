f5486dec372ecbf35a86e413f9df0878bbb87096
Nicollas Anderson <nicollas_sdias@hotmail.com>
2022-03-16 18:06:55 -0300

    fix: input style added

--- src/Components/Input/index.jsx
+++ src/Components/Input/index.jsx
@@ -1,11 +1,13 @@
+import { InputContainer } from "./style";
+
 const Input = ({ register, name, label, error, ...rest }) => {
-    return (
-      <div>
-        <label>{label}</label>
-          <input {...register(name)} {...rest} />
-        <div>{!!error && <span>{error}</span>}</div>
-      </div>
-    )
-  }
-  
-  export default Input;
+  return (
+    <InputContainer>
+      <label>{label}</label>
+      <input {...register(name)} {...rest} />
+      <div>{!!error && <span>{error}</span>}</div>
+    </InputContainer>
+  );
+};
+
+export default Input;
---
+++ src/Components/Input/style.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+  width: 100%;
+
+  label {
+    text-align: left;
+    display: block;
+    width: 85%;
+    margin: 0 auto;
+    font-size: 14px;
+    font-weight: 700;
+  }
+
+  input {
+    width: 85%;
+    background: #c4c4c4;
+    border-radius: 5px;
+    border: none;
+    height: 32px;
+    margin: 13px 0;
+    padding-left: 24px;
+
+    &::placeholder {
+      color: black;
+    }
+  }
+
+  form {
+    margin: auto 0;
+  }
+
+  @media (min-width: 768px) {
+    margin: 12px 0;
+    label {
+      font-size: 20px;
+    }
+    input {
+      height: 46px;
+    }
+  }
+`;
