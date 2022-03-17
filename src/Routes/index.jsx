996f8fd55cc06f85b592150a45c9b433c66fa121
Juan Rego da Silva Rocha <juanrego.s@outlook.com>
2022-03-17 15:35:27 -0300

    feat: routes for game and profile

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,26 +1,31 @@
 import { Switch, Route } from "react-router-dom";
-import SingupForm from "../Components/SingupForm";
 import Login from "../Pages/Login";
 import SignUp from "../Pages/Signup";
 import HomePage from "../Pages/Home";
 import GamesPage from "../Pages/Games";
+import Game from "../Pages/Game";
+import Profile from "../Pages/Profile";
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/">
-        <HomePage/>
+        <HomePage />
       </Route>
       <Route path="/login">
-        <Login/>
+        <Login />
       </Route>
       <Route path="/signup">
-        <SignUp/>
+        <SignUp />
       </Route>
-      <Route path="/dashboard/"></Route>
-      <Route path="/profile"></Route>
+      <Route path="/profile">
+        <Profile />
+      </Route>
       <Route path="/games">
-        <GamesPage/>
+        <GamesPage />
+      </Route>
+      <Route path="/gameInfo/:id">
+        <Game />
       </Route>
     </Switch>
   );
