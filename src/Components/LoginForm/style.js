9e7ac971faa5536b2c6c22f01fd0228f31b232e1
Marcos <marcosmafeimachado@gmail.com>
2022-03-16 21:43:00 -0300

    feat: added new styles for the login form

--- src/Components/LoginForm/style.js
+++ src/Components/LoginForm/style.js
@@ -6,10 +6,12 @@ export const FormDivContainer = styled.div`
   box-shadow: -4px 4px 3px rgba(0, 0, 0, 0.25);
   border-radius: 5px;
   width: 75%;
-  max-width: 500px;
+  min-width:280px;
+  max-width: 400px;
   margin: 46px auto;
   color: var(--white);
   padding: 10px;
+  height: 500px;
 
   h1 {
     font-size: 40px;
@@ -18,15 +20,27 @@ export const FormDivContainer = styled.div`
     color: var(--purple);
   }
 
+  form{
+    padding-top: 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    align-items: center;
+  }
+
   span {
     display: inline-block;
     margin: 15px;
-
+    margin-top: 60px;
     a {
         margin: 0 5px;
         text-decoration: none;
         text-transform: uppercase;
     }
 
+  }
+  @media screen and (min-width: 768px) {
+    height: 600px;
+    max-width: 450px;
   }
 `;
