b42f506ce06dba49198ea4456e9ec27991396bc5
Marcos <marcosmafeimachado@gmail.com>
2022-03-18 14:39:25 -0300

    feat: now using params to render the game page dinamically

--- src/Components/FullCardGame/index.jsx
+++ src/Components/FullCardGame/index.jsx
@@ -7,10 +7,12 @@ import axios from "axios";
 import { toast } from "react-toastify";
 import { useParams } from "react-router-dom";
 
-const FullCardGame = ({grade }) => {
+const FullCardGame = ({ grade }) => {
   const [isClicked, setIsClicked] = useState(false);
-  const {slug} = useParams();
-  const [gameInfo,setGameInfo] = useState([])
+  const { slug } = useParams();
+  const [gameInfo, setGameInfo] = useState([]);
+  const [plataforms,setPlataforms] = useState([]);
+  const [description, setDescription] = useState("");
 
   useEffect(() => {
     axios
@@ -18,10 +20,14 @@ const FullCardGame = ({grade }) => {
         `https://api.rawg.io/api/games/${slug}?key=870a1b01479c4490b54b590b47f030f9`
       )
       .then((response) => {
-        setGameInfo(response.data)
+        setGameInfo(response.data);
+        setPlataforms(response.data.parent_platforms);
+        setDescription(response.data.description)
       })
-      .catch((error) => {toast.error("Ops! Página não encontrada",{theme:"dark"})})
-  },[gameInfo])
+      .catch((error) => {
+        toast.error("Ops! Página não encontrada", { theme: "dark" });
+      });
+  }, [gameInfo]);
 
   const handleClick = () => {
     setIsClicked(!isClicked);
@@ -29,61 +35,63 @@ const FullCardGame = ({grade }) => {
 
   return (
     <>
-      {gameInfo && <><Mobile>
-      <div className="img-holder">
-        <img
-          className="game-image"
-          src={gameInfo.background_image}
-          alt={gameInfo.name}
-        />
-      </div>
-      <div className="info-holder">
-        <div>
-          <span>{gameInfo.name}</span>
-        </div>
-        <div className="game-plataform">
-          { gameInfo && <h4>
-            Plataformas:
-            {gameInfo.parent_platforms.map((platforms) => (
-              <span> {platforms.platform.name}</span>
-            ))}
-          </h4>}
-        </div>
-        <div className="game-description">
-          {/* <div>
-            {gameInfo.description.replace(/[<p>\\/&/;]/g, "")}
-          </div> */}
-          <div className="arrow-buttom" onClick={handleClick}>
-            {isClicked ? <AiOutlineDown /> : <AiOutlineUp />}
-          </div>
-        </div>
-      </div>
-      <>
-        {isClicked ? (
-          <More>
-            <div>
+      {gameInfo && (
+        <>
+          <Mobile>
+            <div className="img-holder">
+              <img
+                className="game-image"
+                src={gameInfo.background_image}
+                alt={gameInfo.name}
+              />
+            </div>
+            <div className="info-holder">
               <div>
-                <img src={gamesHubLogo} alt="GamesHub logo" />
-                <span>{grade}</span>
+                <span>{gameInfo.name}</span>
               </div>
-              <div>
-                <img
-                  src="https://www.metacritic.com/images/icons/metacritic-icon.svg"
-                  alt="metacritic"
-                ></img>
-                <span>{gameInfo.metacritic}</span>
+              <div className="game-plataform">
+                <h4>
+                  Plataformas:
+                  {plataforms.map((platforms,index) => (
+                    <span key={index}> {platforms.platform.name}</span>
+                  ))}
+                </h4>
+              </div>
+              <div className="game-description">
+                <div>{description.replace(/[<p>\\/&/;]/g, "")}</div>
+                <div className="arrow-buttom" onClick={handleClick}>
+                  {isClicked ? <AiOutlineDown /> : <AiOutlineUp />}
+                </div>
               </div>
             </div>
-            <div>
-              <span>Deixe sua nota também</span>
-              <div></div>
-            </div>
-          </More>
-        ) : (
-          <MoreHide></MoreHide>
-        )}
-      </>
-    </Mobile></>}
+            <>
+              {isClicked ? (
+                <More>
+                  <div>
+                    <div>
+                      <img src={gamesHubLogo} alt="GamesHub logo" />
+                      <span>{grade}</span>
+                    </div>
+                    <div>
+                      <img
+                        src="https://www.metacritic.com/images/icons/metacritic-icon.svg"
+                        alt="metacritic"
+                      ></img>
+                      <span>{gameInfo.metacritic}</span>
+                    </div>
+                  </div>
+                  <div>
+                    <span>Deixe sua nota também</span>
+                    <div></div>
+                  </div>
+                </More>
+              ) : (
+                <MoreHide></MoreHide>
+              )}
+            </>
+          </Mobile>
+        </>
+      )}
     </>
   );
 };
