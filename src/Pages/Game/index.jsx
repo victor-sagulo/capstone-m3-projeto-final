f46a489349fe8abe1ba416cec5f70500f8e94839
Marcos <marcosmafeimachado@gmail.com>
2022-03-18 14:05:07 -0300

    fix: fixing the gamesInfo page

--- src/Pages/Game/index.jsx
+++ src/Pages/Game/index.jsx
@@ -1,19 +1,18 @@
 import { GamesContext } from "../../Providers/Games";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import Header from "../../Components/Header";
 import Modals from "../../Components/Modal";
 import FullCardGame from "../../Components/FullCardGame";
 
+
 const Game = () => {
-  const { gameInfo } = useContext(GamesContext);
-  console.log(gameInfo);
 
   return (
     <main>
       <div className="home-top">
         <Header />
       </div>
-      <FullCardGame game={gameInfo} grade={5} />
+      <FullCardGame  grade={5} />
     </main>
   );
 };
