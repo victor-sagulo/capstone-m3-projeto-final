ce08e204f5d4df6634d212a96c0c07fa9f725000
Marcos <marcosmafeimachado@gmail.com>
2022-03-18 14:48:27 -0300

    fix: removed some unused variables

--- src/Pages/Game/index.jsx
+++ src/Pages/Game/index.jsx
@@ -1,15 +1,7 @@
-import { GamesContext } from "../../Providers/Games";
-import { useContext, useEffect, useState } from "react";
 import Header from "../../Components/Header";
-import Modals from "../../Components/Modal";
 import FullCardGame from "../../Components/FullCardGame";
-import CardComment from "../../Components/CardComment";
-import CommentsList from "../../Components/CommentsList";
 
 const Game = () => {
-  /* const arrayOfComments = () => {
-    return postsList.map((post) => post.game === gameInfo.name);
-  }; */
 
   return (
     <main>
