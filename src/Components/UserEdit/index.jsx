f1eac6d7beb8498faaa13298aa4a0940877c6cce
Oswaldo-Deco <oswaldo97@live.com>
2022-03-18 16:33:56 -0300

    Feat: User edit logic was applied

--- src/Components/UserEdit/index.jsx
+++ src/Components/UserEdit/index.jsx
@@ -4,13 +4,17 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
 import Buttons from "../Buttons";
 import { Body, FormDivContainer } from "./style";
+import { useContext } from "react";
+import { UserContext } from "../../Providers/user";
 
 const UserEdit = ({ handleModal }) => {
+  const {user, handleEditUser} = useContext(UserContext)
+
   const schema = yup.object().shape({
-    username: yup.string(),
-    description: yup.string(),
-    img: yup.string(),
+    username: yup.string().max(18),
     plataform: yup.string(),
+    img: yup.string(),
+    description: yup.string(),
   });
 
   const {
@@ -22,11 +26,17 @@ const UserEdit = ({ handleModal }) => {
   });
 
   const handleEdit = (data) => {
-return console.log(data)
+    
+    if(data.username===""){delete data.username}
+    if(data.plataform===""){delete data.plataform}
+    if(data.img===""){delete data.img}
+    if(data.description===""){delete data.description}
+    console.log(data)
+handleEditUser(data)
   }
 
   return (
-        <form onSubmit={handleSubmit()}>
+        <form onSubmit={handleSubmit(handleEdit)}>
           <Input
             name="username"
             register={register}
@@ -36,11 +46,11 @@ return console.log(data)
             type="text"
           />
           <Input
-            name="description"
+            name="plataform"
             register={register}
-            label="Descrição"
-            placeholder="Altere a sua descrição"
-            error={errors.description?.message}
+            label="plataform"
+            placeholder="Altere sua plataforma favorita"
+            error={errors.plataform?.message}
             type="text"
           />
           <Input
@@ -52,12 +62,19 @@ return console.log(data)
             type="text"
           />
           <Input
-            name="plataform"
+            name="description"
             register={register}
-            label="plataform"
-            placeholder="Altere sua plataforma favorita"
-            error={errors.plataform?.message}
+            label="Descrição"
+            placeholder="Altere a sua descrição"
+            error={errors.description?.message}
             type="text"
+          />
+          <Input
+          name="password"
+          register={register}
+          label="Para fazer a alteração coloque sua senha"
+          placeholder="Senha"
+          error={errors.password?.message}
           />
           <div>
             <Buttons type="submit">Alterar</Buttons>
