cb43258aac01e68c0b6d8852ebb208bb7a680910
Victor <vlcampos921@gmail.com>
2022-03-18 14:18:33 -0300

    Fix: general bugs

--- src/Components/CommentsList/index.jsx
+++ src/Components/CommentsList/index.jsx
@@ -1,12 +1,18 @@
 import CardComment from "../CardComment";
 
-const CommentsList = (comments) => {
-
-    return(<div>
-        {comments.map{(comment) =>{
-            <CardComment img={comment.img} name={comment.game} comment={comment.text} likes={comment.likes} />
-        }}
-    </div>)
+const CommentsList = ({ comments }) => {
+  return (
+    <div>
+      {comments.map((comment) => (
+        <CardComment
+          img={comment.img}
+          name={comment.game}
+          comment={comment.text}
+          likes={comment.likes}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default CommentsList;
