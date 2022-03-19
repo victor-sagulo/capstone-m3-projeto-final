dea70c7f2f0f8c1fc177ebde7affbb10c11671f5
Oswaldo-Deco <oswaldo97@live.com>
2022-03-19 15:58:37 -0300

    Feat: Recent comment content added to the page

--- src/Components/RecentComents/index.jsx
+++ src/Components/RecentComents/index.jsx
@@ -1,15 +1,27 @@
 import { BsFillEmojiDizzyFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { UserContext } from "../../Providers/user";
+import { FaThumbsUp } from "react-icons/fa";
+import { useEffect } from "react";
+import CommentsList from "../CommentsList";
 import { DivRecentContainer } from "./style";
 
-const RecentComents = () => {
-  return (
-    <DivRecentContainer>
-      <h3>Comentários</h3>
-      <BsFillEmojiDizzyFill />
-      <p>Não encontramos nenhum comentário seu dos últimos tempos</p>
-    </DivRecentContainer>
-  );
-};
+const RecentComents = ({id}) => {
+const {userPosts}= useContext(UserContext)
 
-export default RecentComents;
+
+return(
+    <DivRecentContainer>
+        <h3 className="comment-tittle">Comentários</h3>
+    {(userPosts.posts).length>0 ? <CommentsList comments={userPosts.posts} profile user={userPosts.username}/>: <div> 
+            <BsFillEmojiDizzyFill className="sad-icon"/>
+            <p className="no-comment-text">Não encontramos nenhum comentário seu</p>
+    </div>
+    }
+</DivRecentContainer>
+)}
+
+
+
+export default RecentComents
