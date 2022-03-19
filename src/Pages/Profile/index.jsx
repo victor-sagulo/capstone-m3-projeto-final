9be3264fabddaca5725ffd09e799dbc695250787
Oswaldo-Deco <oswaldo97@live.com>
2022-03-19 16:15:01 -0300

    FIx:

--- src/Pages/Profile/index.jsx
+++ src/Pages/Profile/index.jsx
@@ -3,8 +3,16 @@ import LikedGames from "../../Components/LikedGames";
 import Header from "../../Components/Header";
 import { ContentWraper, DivProfileContainer } from "./style";
 import RecentComents from "../../Components/RecentComents";
+import { useContext } from "react";
+import { UserContext } from "../../Providers/user";
+import { useParams } from "react-router-dom";
 
 const Profile = () => {
+const {listUserPosts} = useContext(UserContext)
+const {id} = useParams()
+  const iniatiteProfile = () => listUserPosts(id)
+  iniatiteProfile()
+
   return (
     <>
       <Header />
